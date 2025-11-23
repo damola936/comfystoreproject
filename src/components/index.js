ed9a9af78f65179157d47a804e5a5bb39f4cc966
Recompense <damolajimoh100@gmail.com>
2025-11-23 20:22:02 +0100

    Fixed NavBar

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as FormInput } from "./FormInput";
 export { default as SubmitBtn } from "./SubmitBtn";
 export { default as Header } from "./Header";
-export { default as NavBar } from "./Navbar";
+export { default as NavBar } from "./NavBar";
 export { default as Hero } from "./Hero";
 export { default as ErrorElement } from "./ErrorElement";
 export { default as FeaturedProducts } from "./FeaturedProducts";
