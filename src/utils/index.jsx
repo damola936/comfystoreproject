06c61cafc83539137bc1b5ad642f4e4623577bdb
Recompense <damolajimoh100@gmail.com>
2025-11-26 23:09:45 +0100

    Optimization and modification

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -7,11 +7,10 @@ export const customFetch = axios.create({
 });
 
 export const formatPrice = (price) => {
-  const dollarsAmount = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
   }).format((price / 100).toFixed(2));
-  return dollarsAmount;
 };
 
 export const generateAmountOptions = (number) => {
